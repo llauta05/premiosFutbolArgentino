50c8e03608f3c9ea54f2da57dc1590c0804b5fed
Lautaron Landin <llandin@licenciasonline.com>
2022-04-06 19:38:26 -0300

    create form

--- premiosFutbol/polls/views.py
+++ premiosFutbol/polls/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list" : latest_question_list
+    })
+
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question":question})
+
+
+def results(request,question_id):
+    return HttpResponse(f"estas viendo los resultados de la pregunta {question_id}")
+
+def vote(request,question_id):
+    return HttpResponse(f"estas votando a la pregunta {question_id}")
