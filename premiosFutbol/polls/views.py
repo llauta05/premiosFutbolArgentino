fc1bba710511975727fedadb6b225a548c6abf04
Lautaron Landin <llandin@licenciasonline.com>
2022-04-07 12:19:25 -0300

    end project

--- premiosFutbol/polls/views.py
+++ premiosFutbol/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -16,7 +17,17 @@ def detail(request,question_id):
 
 
 def results(request,question_id):
-    return HttpResponse(f"estas viendo los resultados de la pregunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question":question})
 
 def vote(request,question_id):
-    return HttpResponse(f"estas votando a la pregunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {"question":question, "error_message":"no elegiste una opcion"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
